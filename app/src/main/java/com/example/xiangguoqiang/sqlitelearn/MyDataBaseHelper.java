3cb04f5d9b29e493b1de827d860ec5151121178f
xiangguoqiang <xiangguoqiang@baidu.com>
2016-01-11 16:02:27 +0800

    “第一个

---
+++ app/src/main/java/com/example/xiangguoqiang/sqlitelearn/MyDataBaseHelper.java
@@ -0,0 +1,50 @@
+package com.example.xiangguoqiang.sqlitelearn;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+/**
+ * Created by xiangguoqiang on 16/1/11.
+ */
+public class MyDataBaseHelper extends SQLiteOpenHelper {
+
+    public static final String CREATE_BOOK = "create table Book("
+            + "id integer primary key autoincrement, "
+            +"author text, "
+            +"price real, "
+            +"pages integer, "
+            +"name text)";
+
+    public static final String CREATE_CATEGORY = "create table Category("
+            +"id integer primary key autoincrement, "
+            +"category_name text, "
+            +"category_code integer)";
+
+
+    private Context mContext;
+
+    public MyDataBaseHelper(Context context, String name,
+                            SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+        mContext = context;
+    }
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(CREATE_BOOK);
+        sqLiteDatabase.execSQL(CREATE_CATEGORY);
+        Toast.makeText(mContext,"Create successed",Toast.LENGTH_SHORT);
+
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("drop table if exists Book");
+        sqLiteDatabase.execSQL("drop table if exists Category");
+        onCreate(sqLiteDatabase);
+
+    }
+}
